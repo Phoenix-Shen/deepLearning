d3636e5af771162b1bd0e3c8c9bc0dd3be01b21a
whussk <2250257231@qq.com>
2022-05-20 00:13:05 +0800

    feat: :sparkles: CV: AnchorBoxes and ObjectDetection

--- pltutils.py
+++ pltutils.py
@@ -1174,6 +1174,34 @@ def train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs,
           f'{str(devices)}')
 
 
+def box_corner_to_center(boxes: t.Tensor):
+    """从（左上，右下）转换到（中间，宽度，高度）"""
+    x1, y1, x2, y2 = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3]
+    cx = (x1+x2)/2
+    cy = (y1+y2)/2
+    w = x2-x1
+    h = y2-y1
+    boxes = t.stack((cx, cy, w, h), dim=-1)
+    return boxes
+
+
+def box_center_to_corner(boxes: t.Tensor):
+    """从（中间，宽度，高度）转换到（左上，右下）"""
+    cx, cy, w, h = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3]
+    x1 = cx - 0.5*w
+    y1 = cy-0.5*h
+    x2 = cx+0.5*w
+    y2 = cy+0.5*h
+    boxes = t.stack((x1, y1, x2, y2), dim=-1)
+    return boxes
+
+def bbox_to_rect(bbox, color):
+    # 将边界框(左上x,左上y,右下x,右下y)格式转换成matplotlib格式：
+    # ((左上x,左上y),宽,高)
+    return plt.Rectangle(
+        xy=(bbox[0], bbox[1]), width=bbox[2]-bbox[0], height=bbox[3]-bbox[1],
+        fill=False, edgecolor=color, linewidth=2
+    )
 # CONSTANT AND LAMBDA EXPRESSIONS
 numpy = lambda x, *args, **kwargs: x.detach().numpy(*args, **kwargs)
 size = lambda x, *args, **kwargs: x.numel(*args, **kwargs)
