84c5eb29935d67eb1f598bb7fd6de36e2dd8b685
whussk <2250257231@qq.com>
2022-05-19 18:27:43 +0800

    feat: :sparkles: FINETUNING

--- pltutils.py
+++ pltutils.py
@@ -1098,26 +1098,82 @@ class MultiHeadAttention(nn.Module):
         output_concat = transpose_output(output, self.num_heads)
         return self.W_o(output_concat)
 
+
 class PositionalEncoding(nn.Module):
-    def __init__(self,num_hiddens,dropout,max_len=1000):
+    def __init__(self, num_hiddens, dropout, max_len=1000):
         super().__init__()
-        self.dropout=nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout)
         # 创建一个足够长的P
-        self.p = t.zeros((1,max_len,num_hiddens))
-        X = t.arange(max_len,dtype=t.float32).reshape(-1,1)/t.pow(10000,t.arange(0,num_hiddens,2,dtype=t.float32)/num_hiddens)
-        self.p[:,:,0::2]=t.sin(X)
-        self.p[:,:,1::2]=t.cos(X)
-    
-    def forward(self,X):
-        X = X+self.p[:,:X.shape[1],:].to(X.device)
+        self.p = t.zeros((1, max_len, num_hiddens))
+        X = t.arange(max_len, dtype=t.float32).reshape(-1, 1)/t.pow(10000,
+                                                                    t.arange(0, num_hiddens, 2, dtype=t.float32)/num_hiddens)
+        self.p[:, :, 0::2] = t.sin(X)
+        self.p[:, :, 1::2] = t.cos(X)
+
+    def forward(self, X):
+        X = X+self.p[:, :X.shape[1], :].to(X.device)
         return self.dropout.forward(X)
+
+
 class AttentionDecoder(Decoder):
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     @property
     def attention_weights(self):
         raise NotImplementedError
+#@save
+
+
+def train_batch_ch13(net, X, y, loss, trainer, devices):
+    """用多GPU进行小批量训练"""
+    if isinstance(X, list):
+        # 微调BERT中所需（稍后讨论）
+        X = [x.to(devices[0]) for x in X]
+    else:
+        X = X.to(devices[0])
+    y = y.to(devices[0])
+    net.train()
+    trainer.zero_grad()
+    pred = net(X)
+    l = loss(pred, y)
+    l.sum().backward()
+    trainer.step()
+    train_loss_sum = l.sum()
+    train_acc_sum = accuracy(pred, y)
+    return train_loss_sum, train_acc_sum
+
+#@save
+
+
+def train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs,
+               devices=[t.device("cuda:0")]):
+    """用多GPU进行模型训练"""
+    timer, num_batches = Timer(), len(train_iter)
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0, 1],
+                        legend=['train loss', 'train acc', 'test acc'])
+    net = nn.DataParallel(net, device_ids=devices).to(devices[0])
+    for epoch in range(num_epochs):
+        # 4个维度：储存训练损失，训练准确度，实例数，特点数
+        metric = Accumulator(4)
+        for i, (features, labels) in enumerate(train_iter):
+            timer.start()
+            l, acc = train_batch_ch13(
+                net, features, labels, loss, trainer, devices)
+            metric.add(l, acc, labels.shape[0], labels.numel())
+            timer.stop()
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches,
+                             (metric[0] / metric[2], metric[1] / metric[3],
+                              None))
+        test_acc = evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch + 1, (None, None, test_acc))
+    print(f'loss {metric[0] / metric[2]:.3f}, train acc '
+          f'{metric[1] / metric[3]:.3f}, test acc {test_acc:.3f}')
+    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec on '
+          f'{str(devices)}')
+
+
 # CONSTANT AND LAMBDA EXPRESSIONS
 numpy = lambda x, *args, **kwargs: x.detach().numpy(*args, **kwargs)
 size = lambda x, *args, **kwargs: x.numel(*args, **kwargs)
@@ -1144,3 +1200,5 @@ DATA_HUB['time_machine'] = (DATA_URL + 'timemachine.txt',
                             '090b5e7e70c295757f55df93cb0a180b9691891a')
 DATA_HUB['fra-eng'] = (DATA_URL + 'fra-eng.zip',
                        '94646ad1522d915e7b0f9296181140edcf86a4f5')
+DATA_HUB['hotdog'] = (DATA_URL + 'hotdog.zip',
+                      'fba480ffa8aa7e0febbb511d181409f899b9baa5')
